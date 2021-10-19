c9f8fde285b9025472e7e9b60530d7b7e03d297c
haru034 <haru034@icloud.com>
2021-10-19 18:37:37 +0900

    会員編集画面のView完成

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -9,7 +9,29 @@
 </head>
 <body>
     <div class="user-edit">
-        
+        <div class="edit-string">
+            <h2>会員編集 会員ID: {{ $user->id }}</h2> <!--会員IDを取得して各ユーザーのページを表示-->
+        </div>
+        <form action="{{ route('users.update', $user->id) }}" method="post" enctype="multipart/form-data">
+        @csrf
+            <input type="hidden" name="id" value="{{ $user->id }}" /> <!--情報を更新する際に、更新するユーザーIDをサーバーに送る処理-->
+            
+            <div class="user-edit-form">   <!--value属性で初期値(各ユーザーの情報)を表示-->
+                <input type="text" name="name" value="{{ $user->name }}" placeholder="名前" style="width:250px; height:30px;">
+            </div>                             
+            <div class="user-edit-form">  
+                <input type="tel" name="phone" value="{{ $user->phone }}" placeholder="電話番号" style="width:250px; height:30px; margin-top:10px;">
+            </div>
+            <div class="user-edit-form"> 
+                <input type="email" name="email" value="{{ $user->email }}" placeholder="メールアドレス" style="width:250px; height:30px; margin-top:10px;">
+            </div>
+            <div class="user-edit-form">
+                <input type="submit" value="編集" style="width:200px; height:30px; margin-top:20px;">
+            </div>
+            <div class="user-edit-form">
+                <input type="submit" value="削除" style="width:200px; height:30px; margin-top:20px;">
+            </div>
+        </form>
     </div>
 </body>
 </html>
